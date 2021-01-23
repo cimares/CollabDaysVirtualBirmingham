22a8d0792707ead646080a7416b9cda91bbd9407
cimares <cimares@hotmail.com>
2021-01-23 17:50:54 +0000

    Fixed Sponsor header width issue

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 import { useRecoilState } from 'recoil';
 import { ConfigState } from '../states/ConfigState';
 
-export interface ISponsorsProps {}
+export interface ISponsorsProps { }
 
 export const Sponsors: React.FunctionComponent<ISponsorsProps> = (props: ISponsorsProps) => {
-  const [ config ] = useRecoilState(ConfigState);
+  const [config] = useRecoilState(ConfigState);
 
   if (!config || !config.sponsors || config.sponsors.length <= 0) {
     return null;
@@ -13,16 +13,21 @@ export const Sponsors: React.FunctionComponent<ISponsorsProps> = (props: ISponso
 
   return (
     <>
-    <h2 id="sponsors" className="text-center text-sm text-cadetblue-700 tracking-widest font-medium title-font mb-1 p-1 mt-2 bg-gray-200">Featured Sponsors</h2>
-    <section className="container flex flex-col md:flex-row items-center m-auto leading-none mt-8" role="list">
-      
-      {
-        config.sponsors && config.sponsors.filter(s => s.frontPage).map(sponsor => (
-          <a key={sponsor.title.replace(/ /g, '_')} className="sponsor block  md:w-1/3 mb-2 md:mb-0" href={sponsor.url} target="_blank" title={sponsor.title} rel="noopener noreferrer">
-            <img className="w-1/3 mx-auto" src={sponsor.logo} alt={sponsor.title} />
-          </a>
-        ))
-      }
-    </section></>
+      <section className="speakers__overview container items-center m-auto leading-none mt-8" role="list">
+
+        <div className="flex flex-col text-center w-full mb-1 p-1 mt-2">
+          <h2 id="sponsors" className="text-center text-sm text-cadetblue-700 tracking-widest font-medium title-font mb-1 p-1 mt-2 bg-gray-200">Featured Sponsors</h2>
+        </div>
+      </section>
+      <section className="container flex flex-col md:flex-row items-center m-auto leading-none mt-8" role="list">
+
+        {
+          config.sponsors && config.sponsors.filter(s => s.frontPage).map(sponsor => (
+            <a key={sponsor.title.replace(/ /g, '_')} className="sponsor block  md:w-1/3 mb-2 md:mb-0" href={sponsor.url} target="_blank" title={sponsor.title} rel="noopener noreferrer">
+              <img className="w-1/3 mx-auto" src={sponsor.logo} alt={sponsor.title} />
+            </a>
+          ))
+        }
+      </section></>
   );
 };
